a3cbcc528d967a60f748a8e27bdc7174cf236b5d
Bhar1112 <barath11071997@gmail.com>
2025-08-21 14:02:21 +0530

    update the file

---
+++ Human.java
@@ -0,0 +1,50 @@
+class Human{
+
+     String name;
+     int age;
+     char gender;
+     String qualification;
+
+     public void getInformation(){
+  
+ System.out.println( name+" "+age+" "+gender+" "+qualification);
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Student.java
@@ -0,0 +1,23 @@
+  class Student{
+
+    public static void main (String bh[]){
+
+        Human bharath = new Human();
+
+       // bharath.getInformation();
+
+        bharath.name = "Bharath K";
+        bharath.age = 25;
+        bharath.gender = 'M';
+        bharath.qualification = "Msc Maths";
+
+         bharath.getInformation();
+
+         Human mani = new Human();
+         mani.getInformation();
+}
+}
+
+
+
+
